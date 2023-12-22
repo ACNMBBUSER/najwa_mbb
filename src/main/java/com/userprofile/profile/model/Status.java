f56a3f07d5de5ee693bee9a0edbba004b7df19c8
nrainnjwa <njwasshi@gmail.com>
2023-12-22 10:25:39 +0800

    feat: Add custom ResponseHandler

--- src/main/java/com/userprofile/profile/model/Status.java
+++ src/main/java/com/userprofile/profile/model/Status.java
@@ -1,7 +1,55 @@
 package com.userprofile.profile.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
 public class Status {
+
+    @JsonProperty("Type")
+    public String Type;
+    @JsonProperty("Code")
+    public int Code;
+    @JsonProperty("Message")
+    public String Message;
+    @JsonProperty("Timestamp")
+    public long Timestamp;
+    @JsonProperty("Errors")
+    public List<String> Errors;
+
+    public Status(String type, int code, long timestamp, String error) {
+        this.Type = type;
+        this.Code = code;
+        this.Timestamp = timestamp;
+        this.Errors = new ArrayList();
+        this.Errors.add(error);
+    }
+
+    public Status(String type, int code, long timestamp, List<String> errors) {
+        this.Type = type;
+        this.Code = code;
+        this.Timestamp = timestamp;
+        this.Errors = errors;
+    }
+
+    public Status(String Type, int Code, String Message, long Timestamp, List<String> Errors) {
+        this.Type = Type;
+        this.Code = Code;
+        this.Message = Message;
+        this.Timestamp = Timestamp;
+        this.Errors = Errors;
+    }
+
+    public Status() {
+    }
 }
--- src/main/java/com/userprofile/profile/model/response/ResponseHandler.java
+++ src/main/java/com/userprofile/profile/model/response/ResponseHandler.java
@@ -1,4 +1,57 @@
 package com.userprofile.profile.model.response;
 
-public class ResponseHandler {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.userprofile.profile.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@Component
+public class ResponseHandler<T> {
+
+    private static final Logger log = LoggerFactory.getLogger(ResponseHandler.class);
+    @JsonProperty("Status")
+    public Status Status;
+    @JsonProperty("Response")
+    public T Response;
+
+    public ResponseHandler(Status status) {
+        this.Status = status;
+    }
+
+    public ResponseHandler generateFailResponse(String error) {
+        Status status = new Status("FAILED", HttpStatus.INTERNAL_SERVER_ERROR.value(), Instant.now().toEpochMilli(), error);
+        return new ResponseHandler(status, (Object)null);
+    }
+
+    public ResponseHandler generateFailResponse(int statusCode, String error) {
+        Status status = new Status("FAILED", statusCode, Instant.now().toEpochMilli(), error);
+        return new ResponseHandler(status, (Object)null);
+    }
+
+    public ResponseHandler generateSuccessResponse(Object data) {
+        Status status = new Status("OK", HttpStatus.OK.value(), Instant.now().toEpochMilli(), (List)null);
+        return new ResponseHandler(status, data);
+    }
+
+    public ResponseHandler(Status Status, T Response) {
+        this.Status = Status;
+        this.Response = Response;
+    }
+
+    public ResponseHandler() {
+    }
 }
